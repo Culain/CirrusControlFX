1d9ca9c9a2c829258fa272922a12aa42af55eebc
lordfwahfnah <renegade2200@yahoo.de>
2018-04-10 00:32:00 +0200

    added basic functions:
    - send commands
    - button events for the four main buttons

--- src/CirrusControl/Main/CirrusScanner.java
+++ src/CirrusControl/Main/CirrusScanner.java
@@ -1,12 +1,11 @@
 package CirrusControl.Main;
 
-import javafx.beans.property.IntegerProperty;
-
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.*;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
 public class CirrusScanner {
 
@@ -14,73 +13,67 @@ public class CirrusScanner {
     private int port = 20001;
     private String ipAddress = "127.0.0.1";
 
-    public CirrusScanner() {
+    CirrusScanner() {
         assert true;
     }
 
-    public String getIPAddr() {
+    public String getIpAddress() {
         return this.ipAddress;
     }
 
-    public void setIPAddr(String value) {
+    void setIpAddress(String value) {
         try {
-            InetAddress.getByName(value);
+            InetAddress.getByName(value);  //only try to parse. ignore value
             this.ipAddress = value;
         } catch (UnknownHostException e) {
             //ignore
         }
     }
 
-    public Response sendCommand(String command) {
+    Response sendCommand(String command) {
         return sendCommand(command, 0);
     }
 
-    public Response sendCommand(String command, int Master) {
-        Response response = new Response();
+    private Response sendCommand(String command, int Master) {
+        String temp;
         String socket_ip = this.ipAddress;
-        int socket_port = port + Master + multicomserver * 30000;
+        int socket_port = port + Master + multicomserver * 30000; // add 30000 if the MultiCommandServer is used
 
-//        try {
-//            Socket socket = new Socket(socket_ip, socket_port); //add 30000 for multi command server usage
-//            PrintWriter out = new PrintWriter(socket.getOutputStream(),
-//                    true);
-//            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-//
-//            System.out.println(in.toString());
+//        try (
+//                Socket socket = new Socket(socket_ip, socket_port);
+//                PrintWriter out =
+//                        new PrintWriter(socket.getOutputStream(), true);
+//                BufferedReader in =
+//                        new BufferedReader(
+//                                new InputStreamReader(socket.getInputStream()));
+//                BufferedReader stdIn =
+//                        new BufferedReader(
+//                                new InputStreamReader(System.in))
+//        ) {
+//            while ((command = stdIn.readLine()) != null) {
+//                out.println(command);
+//                System.out.println("echo: " + in.readLine());
+//            }
 //        } catch (UnknownHostException e) {
-//            System.out.println("Unknown host: kq6py");
+//            System.err.println("Don't know about host " + ipAddress);
 //            System.exit(1);
 //        } catch (IOException e) {
-//            System.out.println("No I/O");
+//            System.err.println("Couldn't get I/O for the connection to " +
+//                    ipAddress);
 //            System.exit(1);
 //        }
 
-        try (
-                Socket socket = new Socket(socket_ip, socket_port);
-                PrintWriter out =
-                        new PrintWriter(socket.getOutputStream(), true);
-                BufferedReader in =
-                        new BufferedReader(
-                                new InputStreamReader(socket.getInputStream()));
-                BufferedReader stdIn =
-                        new BufferedReader(
-                                new InputStreamReader(System.in))
-        ) {
-            while ((command = stdIn.readLine()) != null) {
-                out.println(command);
-                System.out.println("echo: " + in.readLine());
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Don't know about host " + ipAddress);
-            System.exit(1);
+        try {
+            Socket socket = new Socket(socket_ip, socket_port);
+            Scanner sc = new Scanner(socket.getInputStream());
+            PrintStream p = new PrintStream(socket.getOutputStream());
+            p.println(command);
+            temp = sc.next();
+//            System.out.println(temp);
+            return new Response(temp);
         } catch (IOException e) {
-            System.err.println("Couldn't get I/O for the connection to " +
-                    ipAddress);
-            System.exit(1);
+            e.printStackTrace();
         }
-
-
-        return response;
+        return new Response("ERR");
     }
-
 }
