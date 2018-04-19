68d9e4ccdb8fb767270db0de722cfcc492f44515
Culain <Culain@devtal.de>
2018-04-19 21:34:54 +0200

    added List Entry Class with Control and Response Entrys
    magic

--- src/CirrusControl/Main/Response.java
+++ src/CirrusControl/Main/Response.java
@@ -16,47 +16,6 @@ public class Response {
     private String sendCommand;  //Command that was send to the Scanner
     private String command;      //Command that was received from the Scanner
     private int status = -20;    //Status -20: Unknown Status
-    HashMap<Integer, String> statusList = new HashMap<Integer, String>() {{
-        put(-30, "Connection Timeout");
-        put(-20, "CirrusControlFX: No Status Message found");
-        put(-10, "CirrusControlFX: Error Message from Scanner");
-        put(-2, "RPL3D command failed (Not ok)");
-        put(-1, "RPL3D Unknown");
-        put(0, "RPL3D Command successful (All ok)");
-        put(1, "RPL3D Communication error");
-        put(2, "RPL3D Internal error 2");
-        put(3, "RPL3D Invalid model configuration file path (.cfg)");
-        put(4, "RPL3D Invalid input points");
-        put(5, "RPL3D Unknown model ID");
-        put(6, "RPL3D Corrupted model file");
-        put(7, "RPL3D Unloaded Model");
-        put(8, "RPL3D Invalid model file path (.3xx)");
-        put(9, "RPL3D Internal error 9");
-        put(10, "RPL3D Timeout - Calculation time exceeded");
-        put(11, "RPL3D Unsupported function");
-        put(100, "RPL3D Internal error 100");
-        put(101, "RPL3D Invalid input scan ID");
-        put(102, "RPL3D Internal error 102");
-        put(103, "RPL3D Internal error 103");
-        put(201, "RPL3D Script compilation error");
-        put(202, "RPL3D Sript execution error (Please check RPL3D console)");
-        put(1000, "RPL3D No localization : not enough points");
-        put(1001, "RPL3D No localization : not localizable part");
-        put(1002, "RPL3D No localization : localization out of tolerance");
-        put(1003, "RPL3D No localization : converging localization");
-        put(1004, "RPL3D No localization : orientation contraints not ok");
-        put(1005, "RPL3D No localization : volumic estimation not ok");
-        put(1006, "RPL3D No localization : surfacic estimation not ok");
-        put(2000, "RPL3D All parts overlapped");
-        put(3000, "RPL3D Zero gripper above part");
-        put(3001, "RPL3D Gripper collision with lower plane");
-        put(3002, "RPL3D Gripper collision");
-        put(90001, "Bridge Communication error with the HardwareInterface");
-        put(90002, "Bridge No cloud of points receive");
-        put(90003, "Status Unknown");
-        put(90004, "RPL3D Invalid license");
-        put(90005, "RPL3D Invalid Gripper Id");
-    }};
     private int nbConfig;
     private int[] configID;
     private float avg;
@@ -119,6 +78,48 @@ public class Response {
         add("CALC");
     }};
 
+    HashMap<Integer, String> statusList = new HashMap<Integer, String>() {{
+        put(-30, "Connection Timeout");
+        put(-20, "CirrusControlFX: No Status Message found");
+        put(-10, "CirrusControlFX: Error Message from Scanner");
+        put(-2, "RPL3D command failed (Not ok)");
+        put(-1, "RPL3D Unknown");
+        put(0, "RPL3D Command successful (All ok)");
+        put(1, "RPL3D Communication error");
+        put(2, "RPL3D Internal error 2");
+        put(3, "RPL3D Invalid model configuration file path (.cfg)");
+        put(4, "RPL3D Invalid input points");
+        put(5, "RPL3D Unknown model ID");
+        put(6, "RPL3D Corrupted model file");
+        put(7, "RPL3D Unloaded Model");
+        put(8, "RPL3D Invalid model file path (.3xx)");
+        put(9, "RPL3D Internal error 9");
+        put(10, "RPL3D Timeout - Calculation time exceeded");
+        put(11, "RPL3D Unsupported function");
+        put(100, "RPL3D Internal error 100");
+        put(101, "RPL3D Invalid input scan ID");
+        put(102, "RPL3D Internal error 102");
+        put(103, "RPL3D Internal error 103");
+        put(201, "RPL3D Script compilation error");
+        put(202, "RPL3D Sript execution error (Please check RPL3D console)");
+        put(1000, "RPL3D No localization : not enough points");
+        put(1001, "RPL3D No localization : not localizable part");
+        put(1002, "RPL3D No localization : localization out of tolerance");
+        put(1003, "RPL3D No localization : converging localization");
+        put(1004, "RPL3D No localization : orientation contraints not ok");
+        put(1005, "RPL3D No localization : volumic estimation not ok");
+        put(1006, "RPL3D No localization : surfacic estimation not ok");
+        put(2000, "RPL3D All parts overlapped");
+        put(3000, "RPL3D Zero gripper above part");
+        put(3001, "RPL3D Gripper collision with lower plane");
+        put(3002, "RPL3D Gripper collision");
+        put(90001, "Bridge Communication error with the HardwareInterface");
+        put(90002, "Bridge No cloud of points receive");
+        put(90003, "Status Unknown");
+        put(90004, "RPL3D Invalid license");
+        put(90005, "RPL3D Invalid Gripper Id");
+    }};
+
     private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
 
     public String getCreationTime() {
@@ -131,7 +132,6 @@ public class Response {
                 "\t\tTime to Receive: %sms", rawData, statusMessage, NumberFormat.getNumberInstance().format(this.timeToSend));
     }
 
-    @Test
     private void parse(@NotNull String rawData) {
         if (rawData == null || rawData.isEmpty()) throw new IllegalArgumentException();
 
@@ -176,6 +176,7 @@ public class Response {
         }
         if (sendCommand == "STS 1") {
             licenseTime = Integer.parseInt(split_data);
+            status = 0;
         }
     }
 
